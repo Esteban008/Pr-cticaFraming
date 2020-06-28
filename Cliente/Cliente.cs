8db306cc3323091cd0c41e68e9fd6a6cf76d1e84
ESTEBAN LEANDRO MACHADO OBANDO <esteban.machado@epn.edu.ec>
2020-06-28 16:00:22 -0500

    Finalizado

--- Cliente/Cliente.cs
+++ Cliente/Cliente.cs
@@ -6,6 +6,8 @@
 // Resultados:
 //* El codigo permite verificar el funcionamiento de
 // la codificacion en binario y en forma de texto
+// Ejecuta el programa, ¿qué puedes decir sobre las dos codificaciones?
+// Modifica el programa para enviar dos elementos y recibir dos elementos.
 // Conclusiones:
 //*
 // Recomendaciones:
@@ -27,31 +29,60 @@ namespace Cliente
     {
         static void Main(string[] args)
         {
+            //Tiempo que pasa dormido el cliente
             Thread.Sleep(500);
 
+            //Creamos el socket para poder comunicarnos con el servidor
             IPAddress servidor = IPAddress.Parse("127.0.0.1");
             int puerto = 8080;
             IPEndPoint extremo = new IPEndPoint(servidor, puerto);
 
+            //Creamos un cliente TCP y nos conectamos
             TcpClient cliente = new TcpClient();
             cliente.Connect(extremo);
 
+            //Nos proporciona la secuencia de datos para el acceso a la red
             NetworkStream flujoRed = cliente.GetStream();
 
-            Elemento elemento = new Elemento(1234567890987654L, "Cadena de Bicicleta", 18, 1000, true, false);
+            Elemento elemento1 = new Elemento(1234567890987654L, "Cadena de Bicicleta", 18, 1000, true, false);
 
+            //Modifiacion para enviar dos elementos
+
+            Elemento elemento2 = new Elemento(987654321012345L, "Casco de Bicicleta", 20, 45, true, true);
+
+
+            //Utilizamos la libreria que creamos con el metodo Codificador Texto
             CodificadorTexto codificador = new CodificadorTexto();
-            byte[] datosCodificados = codificador.Codificar(elemento);
-            Console.WriteLine("Enviando elemento codificado en texto (" +
-            datosCodificados.Length + " bytes): ");
-            Console.WriteLine(elemento);
+            byte[] datosCodificados = codificador.Codificar(elemento1);
+            Console.WriteLine("Enviando elemento codificado en texto (" +datosCodificados.Length + " bytes): ");
+            Console.WriteLine(elemento1);
 
+            NetworkStream flujoRed2 = cliente.GetStream();
+
+            CodificadorTexto codificador1 = new CodificadorTexto();
+            byte[] datosCodificados1 = codificador1.Codificar(elemento2);
+            Console.WriteLine("Enviando elemento codificado en texto (" + datosCodificados1.Length + " bytes): ");
+            Console.WriteLine(elemento2);
+            
+            //Procedemos a enviar los datos por la red
+            
             flujoRed.Write(datosCodificados, 0, datosCodificados.Length);
+            flujoRed2.Write(datosCodificados1, 0, datosCodificados1.Length);
+
+            //Como tenemos un servidor ECO, ahora tenemos que realizar el rproceso contrario, el decodificador.
+            
             DecodificadorBinario decodificador = new DecodificadorBinario();
             Elemento elementoRecibido = decodificador.Decodificar(cliente.GetStream());
 
+            DecodificadorBinario decodificador1 = new DecodificadorBinario();
+            Elemento elementoRecibido1 = decodificador1.Decodificar(cliente.GetStream());
+
+            //Impresion del texto recibido
             Console.WriteLine("Se recibio un elemento codificado en formato binario:");
             Console.WriteLine(elementoRecibido);
+
+            Console.WriteLine("Se recibio un elemento codificado en formato binario:");
+            Console.WriteLine(elementoRecibido1);
 
             flujoRed.Close();
             cliente.Close();
--- Servidor/Servidor.cs
+++ Servidor/Servidor.cs
@@ -8,6 +8,12 @@
 //* El codigo permite verificar el funcionamiento de
 // la codificacion en binario y en forma de texto
 // 
+// Ejecuta el programa, ¿qué puedes decir sobre las dos codificaciones?
+// El metodo de codificacion varia dependiendo del elemento a enviar, y al momento 
+// de reenviarlo en el servior igual se realiza una modificacion para poder enviarlo
+// lo cual nos permite darnos cuenta como trabaja la libreria codificador con los 
+// diferentes tipos de mensajes.
+// Modifica el programa para enviar dos elementos y recibir dos elementos.
 // Conclusiones:
 //*
 // Recomendaciones:
@@ -28,25 +34,45 @@ namespace Servidor
     {
         static void Main(string[] args)
         {
+            //Creacion del socket en el que vamos a escuchar
             int puerto = 8080;
             TcpListener socketEscucha = new TcpListener(IPAddress.Any, puerto);
             socketEscucha.Start();
 
+            //Aceptamos al cliente
             TcpClient cliente = socketEscucha.AcceptTcpClient();
 
+            //Utilizamos la libreria Codificador para poder Decifrar el texto
             DecodificadorTexto decodificador = new DecodificadorTexto();
             Elemento elemento = decodificador.Decodificar(cliente.GetStream());
 
+            //Se muestra el mensaje cifrado
             Console.WriteLine("Se recibio un elemento codificado en texto:");
             Console.WriteLine(elemento);
 
+            DecodificadorTexto decodificador1 = new DecodificadorTexto();
+            Elemento elemento1 = decodificador1.Decodificar(cliente.GetStream());
+
+            Console.WriteLine("Se recibio un elemento codificado en texto:");
+            Console.WriteLine(elemento1);
+            //Se codifica de nuevo el mensaje para su renvio 
             CodificadorBinario codificador = new CodificadorBinario();
             elemento.precio += 10;
 
+            CodificadorBinario codificador1 = new CodificadorBinario();
+            elemento1.precio += 10;
+
+            //Se envia al cliente el mensaje nuevamente codificado.
             Console.Write("Enviando elemento en binario...");
             byte[] bytesParaEnviar = codificador.Codificar(elemento);
             Console.WriteLine("(" + bytesParaEnviar.Length + " bytes): ");
             cliente.GetStream().Write(bytesParaEnviar, 0, bytesParaEnviar.Length);
+
+            Console.Write("Enviando elemento en binario...");
+            byte[] bytesParaEnviar1 = codificador1.Codificar(elemento1);
+            Console.WriteLine("(" + bytesParaEnviar1.Length + " bytes): ");
+            cliente.GetStream().Write(bytesParaEnviar1, 0, bytesParaEnviar1.Length);
+
 
             cliente.Close();
 
