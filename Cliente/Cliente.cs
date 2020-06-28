52aa3746066ac4aa54bf796cf7fef35fe30e80f8
ESTEBAN LEANDRO MACHADO OBANDO <esteban.machado@epn.edu.ec>
2020-06-28 01:32:27 -0500

    Agregar archivos de proyecto.

---
+++ Cliente/Cliente.cs
@@ -0,0 +1,61 @@
+﻿// ************************************************************************
+// Practica 03
+// Joseph Bravo, Esteban Machado
+// Fecha de realizacion: 20/06/2020
+// Fecha de entrega: 30/06/2020
+// Resultados:
+//* El codigo permite verificar el funcionamiento de
+// la codificacion en binario y en forma de texto
+// Conclusiones:
+//*
+// Recomendaciones:
+//*
+// ************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Codificador;
+
+namespace Cliente
+{
+    class Cliente
+    {
+        static void Main(string[] args)
+        {
+            Thread.Sleep(500);
+
+            IPAddress servidor = IPAddress.Parse("127.0.0.1");
+            int puerto = 8080;
+            IPEndPoint extremo = new IPEndPoint(servidor, puerto);
+
+            TcpClient cliente = new TcpClient();
+            cliente.Connect(extremo);
+
+            NetworkStream flujoRed = cliente.GetStream();
+
+            Elemento elemento = new Elemento(1234567890987654L, "Cadena de Bicicleta", 18, 1000, true, false);
+
+            CodificadorTexto codificador = new CodificadorTexto();
+            byte[] datosCodificados = codificador.Codificar(elemento);
+            Console.WriteLine("Enviando elemento codificado en texto (" +
+            datosCodificados.Length + " bytes): ");
+            Console.WriteLine(elemento);
+
+            flujoRed.Write(datosCodificados, 0, datosCodificados.Length);
+            DecodificadorBinario decodificador = new DecodificadorBinario();
+            Elemento elementoRecibido = decodificador.Decodificar(cliente.GetStream());
+
+            Console.WriteLine("Se recibio un elemento codificado en formato binario:");
+            Console.WriteLine(elementoRecibido);
+
+            flujoRed.Close();
+            cliente.Close();
+
+        }
+    }
+}
---
+++ Servidor/Servidor.cs
@@ -0,0 +1,56 @@
+﻿// ************************************************************************
+// Practica 03
+// Joseph Bravo, Esteban Machado
+// Fecha de realizacion: 20/06/2020
+// Fecha de entrega: 30/06/2020
+// Resultados:
+//* El codigo permite verificar el funcionamiento de
+//* El codigo permite verificar el funcionamiento de
+// la codificacion en binario y en forma de texto
+// 
+// Conclusiones:
+//*
+// Recomendaciones:
+//*
+// ************************************************************************
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using Codificador;
+
+namespace Servidor
+{
+    class Servidor
+    {
+        static void Main(string[] args)
+        {
+            int puerto = 8080;
+            TcpListener socketEscucha = new TcpListener(IPAddress.Any, puerto);
+            socketEscucha.Start();
+
+            TcpClient cliente = socketEscucha.AcceptTcpClient();
+
+            DecodificadorTexto decodificador = new DecodificadorTexto();
+            Elemento elemento = decodificador.Decodificar(cliente.GetStream());
+
+            Console.WriteLine("Se recibio un elemento codificado en texto:");
+            Console.WriteLine(elemento);
+
+            CodificadorBinario codificador = new CodificadorBinario();
+            elemento.precio += 10;
+
+            Console.Write("Enviando elemento en binario...");
+            byte[] bytesParaEnviar = codificador.Codificar(elemento);
+            Console.WriteLine("(" + bytesParaEnviar.Length + " bytes): ");
+            cliente.GetStream().Write(bytesParaEnviar, 0, bytesParaEnviar.Length);
+
+            cliente.Close();
+
+            socketEscucha.Stop();
+        }
+    }
+}
